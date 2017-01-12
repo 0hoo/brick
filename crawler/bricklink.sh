a4c26feba20afc8c5510d40a290989644fc9ff7e
Kim Younghoo <zerohoo.kim@gmail.com>
2017-01-12 18:48:01 -0500

    sh 업데이트2

--- crawler/bricklink.sh
+++ crawler/bricklink.sh
@@ -4,6 +4,6 @@ PATH=$PATH:/usr/local/bin
 export PATH
 scrapy crawl bricklink
 scrapy crawl ebay
-python3 ../bricksite/scripts/run_ebay_history.py
+python3 ../bricksite/scripts/run_ebay_record.py
 python3 ../bricksite/scripts/run_update_dashboard.py
 ls -al >> x.log
