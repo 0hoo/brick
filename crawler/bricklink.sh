dc0a166904cf5b04144e695259cd74a1a8fb6198
Kim Younghoo <zerohoo.kim@gmail.com>
2017-01-12 18:45:20 -0500

    sh 업데이트

--- crawler/bricklink.sh
+++ crawler/bricklink.sh
@@ -1,9 +1,9 @@
 #! /bin/bash
-cd /home/tmb/lego/legocrawler
+cd /home/tmb/brick/crawler
 PATH=$PATH:/usr/local/bin
 export PATH
 scrapy crawl bricklink
 scrapy crawl ebay
-python3 ../legosite/scripts/run_ebay_history.py
-python3 ../legosite/scripts/run_update_dashboard.py
+python3 ../bricksite/scripts/run_ebay_history.py
+python3 ../bricksite/scripts/run_update_dashboard.py
 ls -al >> x.log
