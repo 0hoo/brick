b89f805058d77ed6f4db6fd85fec7d28894f1c52
Kim Younghoo <zerohoo.kim@gmail.com>
2017-01-14 01:14:53 -0500

    fix theme url

--- bricksite/products/urls.py
+++ bricksite/products/urls.py
@@ -6,7 +6,7 @@ app_name = 'products'
 
 urlpatterns = [
     url(r'^$', views.ProductListView.as_view(), name='list'),
-    url(r'^/theme/(?P<theme_title>[^/]+)/$', views.ProductListView.as_view(), name='list_by_theme'),
+    url(r'^theme/(?P<theme_title>[^/]+)/$', views.ProductListView.as_view(), name='list_by_theme'),
     url(r'^search/add/$', views.ProductSearchForAddView.as_view(), name='search'),
     url(r'^(?P<pk>[0-9]+)/$', views.ProductDetailView.as_view(), name='detail'),
 ]
