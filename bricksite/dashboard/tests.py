4e66c83a4b6fd4cbcf136b7f825a34fe438f4c8c
Kim Younghoo <zerohoo.kim@gmail.com>
2017-02-20 12:35:40 +0900

    dashboard 테스트 통과

--- bricksite/dashboard/tests.py
+++ bricksite/dashboard/tests.py
@@ -16,7 +16,7 @@ class DashboardTests(TestCase):
     def setUp(self):
         self.user = User.objects.create_user(username='tmb', email='tmb@trackmybrick.com', password='tmb')
         BrickSet.objects.create(brick_code=1, title='Test Product', official_price=10.0)
-        self.product = BrickSet.objects.get(brick_code=1)
+        self.brickset = BrickSet.objects.get(brick_code=1)
 
     def test_snapshot_last_dashboard_with_no_items(self):
         dashboard = snapshot_latest_dashboard(self.user)
@@ -42,10 +42,10 @@ class DashboardTests(TestCase):
         self.assertEqual(dashboard.target_at, datetime.utcnow().date())
 
     def test_snapshot_last_dashboard_values(self):
-        Item.objects.create(product=self.product, user=self.user)
-        item = Item.objects.get(product=self.product)
-        MyBrickItem.objects.create(item=item, buying_price=8.0)
-        MyBrickItem.objects.create(item=item, buying_price=9.0)
+        MyBrick.objects.create(brickset=self.brickset, user=self.user)
+        mybrick = MyBrick.objects.get(brickset=self.brickset)
+        MyBrickItem.objects.create(mybrick=mybrick, buying_price=8.0)
+        MyBrickItem.objects.create(mybrick=mybrick, buying_price=9.0)
 
         dashboard = snapshot_latest_dashboard(self.user)
         self.assertEqual(Dashboard.objects.count(), 1)
@@ -56,9 +56,9 @@ class DashboardTests(TestCase):
         self.assertEqual(dashboard.total_official_price, 20)
         self.assertEqual(dashboard.total_profit, float(dashboard.total_estimated_price) - dashboard.total_buying_price)
 
-        BricklinkRecord.objects.create(product=self.product, new_average_price=15.0)
-        EbayRecord.objects.create(product=self.product, used_average_price=12.5)
-        MyBrickItem.objects.create(item=item, buying_price=7.0, opened=True)
+        BricklinkRecord.objects.create(brickset=self.brickset, new_average_price=15.0)
+        EbayRecord.objects.create(brickset=self.brickset, used_average_price=12.5)
+        MyBrickItem.objects.create(mybrick=mybrick, buying_price=7.0, opened=True)
 
         dashboard = snapshot_latest_dashboard(self.user)
         self.assertEqual(Dashboard.objects.count(), 1)
